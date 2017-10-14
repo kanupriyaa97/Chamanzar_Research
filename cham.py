14a5d56616b4891474f1ca77be84f0e45ec62bfe
kanupriyaa97 <kanupriyaa_star@yahoo.com>
2017-10-15 00:19:08 +0300

    Basic changes

--- cham.py
+++ cham.py
@@ -4,61 +4,29 @@ eng = matlab.engine.start_matlab()
 
 def main():
     # Create the webcam object. 
-    cam = webcam()
+    cam = webcam(1)
 
     # Capture one frame to get its size.
-    vdfrm = snapshot(cam)
+    #vdfrm = snapshot(cam)
 
     # Create the video player object.
-    videoPlayer = vision.VideoPlayer('Position', [100, 100 [frameSize(2), frameSize(1)]+30])
+    #videoPlayer = vision.VideoPlayer('Position', [100, 100 [frameSize(2), frameSize(1)]+30])
 
-    runLoop = true
+    #runLoop = true
 
-    while (runLoop):
+    while (True):
         # Get the next frame.
-        vdfrm = snapshot(cam)
-                imdata = imread(vdfrm);
-                imdata(:,:,1) = 0;
-                image(imdata);
-
+        imdata = snapshot(cam)
+                #imdata = imread(vdfrm);
+        imdata(:,:,1) = 0;
+                #image(imdata);
+        imshow(imdata)
         # Display the annotated video frame using the video player object.
-        step(videoPlayer, imdata)
+        #step(videoPlayer, imdata)
 
         # Check whether the video player window has been closed.
-        runLoop = isOpen(videoPlayer)
+        #runLoop = isOpen(videoPlayer)
 
     # Clean up.
     clear(cam)
-    release(videoPlayer)
-import image
-import matlab.engine
-eng = matlab.engine.start_matlab()
-
-def main():
-    # Create the webcam object. 
-    cam = webcam()
-
-    # Capture one frame to get its size.
-    vdfrm = snapshot(cam)
-
-    # Create the video player object.
-    videoPlayer = vision.VideoPlayer('Position', [100, 100 [frameSize(2), frameSize(1)]+30])
-
-    runLoop = true
-
-    while (runLoop):
-        # Get the next frame.
-        vdfrm = snapshot(cam)
-                imdata = imread(vdfrm);
-                imdata(:,:,1) = 0;
-                image(imdata);
-
-        # Display the annotated video frame using the video player object.
-        step(videoPlayer, imdata)
-
-        # Check whether the video player window has been closed.
-        runLoop = isOpen(videoPlayer)
-
-    # Clean up.
-    clear(cam)
-    release(videoPlayer)
+    #release(videoPlayer)
