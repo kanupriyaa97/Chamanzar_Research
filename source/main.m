52e2adf428bcff7ef041a7c2867944a884269ce6
kanupriyaa97 <kanupriyaa_star@yahoo.com>
2017-10-23 08:30:43 +0300

    Matlab development

--- source/main.m
+++ source/main.m
@@ -1,18 +1,40 @@
 function main()
+    imnum = 0;
     cam = webcam(1);
-	  
     while 1
-      
-        % Get the next frame.
         imdata = snapshot(cam);
-        
-        % Strip of red component
 		imdata(:,:,1) = 0;
-
-        % Display the annotated video frame using the video player object.
         imshow(imdata);
+        main2;
+        if(ans == 102)
+            freeze(imdata, imnum);
+            %return;
+        end
+    end 
+    clear cam;
+end
+
+
+function freeze(imdata,imnum)
+    while 1
+        imshow(imdata);
+        main2;
+        if (ans == 117)
+            return;
+        end
+        if (ans == 115)
+            take_pic(imnum,imdata);
+            imnum = imnum + 1;
+        end
     end
-        
-    % Clean up.
-    clear(cam);
+end
+
+function take_pic(imnum, imdata)
+
+    savepath = 'C:\Users\Kanupriyaa\Desktop';
+    nametemplate = 'image_%04d.png';
+    thisfile = sprintf(nametemplate, imnum);
+    fullname = fullfile(savepath, thisfile);
+    imwrite(imdata, fullname);
+
 end
